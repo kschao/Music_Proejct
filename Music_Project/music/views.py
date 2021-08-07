95e7797638fa5b13a6dc4318841f1485f6f74424
Dustin Koy <dustin.koy@Dustins-MacBook-Pro.local>
2021-08-07 15:13:04 -0500

    fixed urls and view.py

--- Music_Project/music/views.py
+++ Music_Project/music/views.py
@@ -19,3 +19,31 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SongDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        song = self.get_object(pk)
+        if song.do_you_like_the_song:
+            song.likes = 1 + int(song.likes)
+        serializer = SongSerializer(song, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        song = self.get_object(pk)
+        serializer = SongSerializer(song)
+        song.delete()
+        return Response(serializer.data) 
